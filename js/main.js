86f7739b82879451af0bf46493c1abf70f7e914e
Rohan <rk8222419@gmail.com>
2020-01-01 00:14:00 +0530

    fixed navbar collapse bug

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
-$(function() {
-        var selectedClass = "";
-        $(".filter").click(function(){
-        selectedClass = $(this).attr("data-rel");
-        $("#gallery").fadeTo(100, 0.1);
-        $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
-        setTimeout(function() {
-        $("."+selectedClass).fadeIn().addClass('animation');
-        $("#gallery").fadeTo(300, 1);
-        }, 300);
-        });
-        });
+
+        var myVideo = document.getElementById("mute-video"); 
+
+        
+                function mute() { 
+                myVideo.muted=true;
+                } 
+                
+                function unmute() {
+                        myVideo.muted=false;
+                }
