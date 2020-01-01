b4c906d2d1077b531d361de1532c892bebc0493e
Rohan <rk8222419@gmail.com>
2020-01-01 10:16:38 +0530

    finally added a single button to mute unmute video

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 
-        var myVideo = document.getElementById("mute-video"); 
+var myVideo = document.getElementById("mute-video"); 
 
         
-                function mute() { 
-                myVideo.muted=true;
-                } 
-                
-                function unmute() {
-                        myVideo.muted=false;
-                }
+//function changing muted value using single buttton.
+
+function sound(){
+        if (myVideo.muted === false) {
+          myVideo.muted = true;
+          // document.test.submit();
+        } else {
+          myVideo.muted = false;
+        }
+  }
